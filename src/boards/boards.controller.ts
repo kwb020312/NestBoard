6bcdd7b4c8d5a67e51f68af09699ba1cb324061f
kwb020312 <kwb020312@naver.com>
2021-08-26 00:06:12 +0900

    #3.4 게시물 생성하기 Controller

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './boards.module';
 import { BoardsService } from './boards.service';
 
@@ -10,6 +10,11 @@ export class BoardsController {
     @Get('/')
     getAllBoard(): Board[] {
         return this.boardsService.getAllBoards()
+    }
+
+    @Post()
+    createBoard(@Body('title') title:string, @Body('description') description: string): Board {
+        return this.boardsService.createBoard(title, description)
     }
     
 }
