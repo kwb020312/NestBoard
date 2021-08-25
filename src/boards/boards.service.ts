a464c9f7060be1026cd38aa0dbf20712a13bc2be
kwb020312 <kwb020312@naver.com>
2021-08-25 22:54:09 +0900

    #3.3 게시물 생성하기 Service 부분

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './boards.module';
+import {v1 as uuid} from 'uuid'
+import { Board, BoardStatus } from './boards.module';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,16 @@ export class BoardsService {
 
     getAllBoards(): Board[] {
         return this.boards
+    }
+
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+        this.boards.push(board)
+        return board
     }
 }
