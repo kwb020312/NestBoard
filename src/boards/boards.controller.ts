711c501f5d1825b9faae3653f9886ef7a5a7d28b
kwb020312 <kwb020312@naver.com>
2021-08-28 16:30:42 +0900

    #4.5 커스텀 파이프를 이용한 유효성 체크

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, Validation
 import { Board, BoardStatus } from './boards.module';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -30,7 +31,7 @@ export class BoardsController {
     }
 
     @Patch('/:id/status')
-    updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus): Board {
+    updateBoardStatus(@Param('id') id: string, @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board {
         return this.boardsService.updateBoardStatus(id,status)
     }
 }
