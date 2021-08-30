0166232c19ec625ec9a908fa1f2c6a25dde1d517
kwb020312 <kwb020312@naver.com>
2021-08-31 00:04:48 +0900

    #6.2 ID를 이용해서 특정 게시물 가져오기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,25 +1,21 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
-    // constructor(private boardsService: BoardsService) {
+    constructor(private boardsService: BoardsService) {
 
-    // }
-    // @Get('/')
-    // getAllBoard(): Board[] {
-    //     return this.boardsService.getAllBoards()
-    // }
-
-    // @Post()
-    // @UsePipes(ValidationPipe)
-    // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    //     return this.boardsService.createBoard(createBoardDto)
-    // }
-    
+        
+    }   
+    @Get('/:id')
+    getBoardById(@Param('id') id:number) :Promise<Board> {
+        return this.boardsService.getBoardById(id)
+    }
+}
     // @Get("/:id")
     // getBoardById(@Param('id') id: string): Board {
     //     return this.boardsService.getBoardById(id)
@@ -34,4 +30,3 @@ export class BoardsController {
     // updateBoardStatus(@Param('id') id: string, @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board {
     //     return this.boardsService.updateBoardStatus(id,status)
     // }
-}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import {v1 as uuid} from 'uuid'
-import { BoardStatus } from './board-status.enum';
-import { CreateBoardDto } from './dto/create-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
+import { BoardRepository } from './board.repository';
 
 @Injectable()
 export class BoardsService {
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository: BoardRepository
+    ){
 
+    }
     // getAllBoards(): Board[] {
     //     return this.boards
     // }
@@ -21,6 +26,16 @@ export class BoardsService {
     //     this.boards.push(board)
     //     return board
     // }
+
+    async getBoardById(id: number): Promise <Board> {
+        const found = await this.boardRepository.findOne(id)
+
+        if(!found) {
+            throw new NotFoundException(`Can't find Board with id ${id}`)
+        }
+
+        return found
+    }
 
     // getBoardById(id: string): Board {
     //     const found = this.boards.find(board => board.id === id)
