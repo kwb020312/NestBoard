9943226f5bf8b47110dd0dcb425d3020c6cbdd4d
kwb020312 <kwb020312@naver.com>
2021-08-31 22:02:49 +0900

    #6.5 게시물 상태 업데이트하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -26,6 +26,14 @@ export class BoardsController {
     deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
         return this.boardsService.deleteBoard(id)
     }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    ): Promise<Board> {
+        return this.boardsService.updateBoardStatus(id, status)
+    }
 }
     // @Get("/:id")
     // getBoardById(@Param('id') id: string): Board {
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -43,6 +43,14 @@ export class BoardsService {
         return this.boardRepository.createBoard(createBoardDto)
     }
 
+    async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+        const board = await this.getBoardById(id)
+        board.status = status
+        await this.boardRepository.save(board)
+
+        return board
+    }
+
     async getBoardById(id: number): Promise <Board> {
         const found = await this.boardRepository.findOne(id)
 
