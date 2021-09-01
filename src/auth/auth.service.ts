d19f67cf65049dbbda7c4e5b5b24987707db52e6
kwb020312 <kwb020312@naver.com>
2021-09-01 21:54:14 +0900

    #7.2 회원가입 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 
@@ -9,4 +10,8 @@ export class AuthService {
         @InjectRepository(User)
         private userRepository: UserRepository 
     ) {}
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.createUser(authCredentialsDto)
+    }
 }
